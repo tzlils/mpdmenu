266084e491495fd01d1a6bf88c5893d0de4a806d
Tzlil <me@tzlil.club>
2021-03-03 01:49:38 +0200

    add support for tracks with no AlbumArtist, but with Artist tags, and support for tracks that dont have an album or an artist tag at all

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
 	"os/exec"
 	"strings"
 
@@ -12,36 +11,60 @@ import (
 )
 
 const (
-	Seperator = " — "
+	Seperator     = " — "
+	UnknownArtist = "Unknown Artist"
+	UnknownAlbum  = "Unknown Album"
 )
 
 func main() {
 	client, err := mpd.Dial("tcp", "localhost:6600")
-	orPanic(err)
+	if err != nil {
+		panic(err)
+	}
 	defer client.Close()
 
 	var lines []string
 	Tracks, err := client.ListAllInfo("/")
-	orPanic(err)
+	if err != nil {
+		panic(err)
+	}
 
 	var AlbumsFound []string
 	var ArtistsFound []string
 	for _, Track := range Tracks {
-		if len(Track["Artist"]) == 0 {
-			continue
+		var Artist string
+		if len(Track["AlbumArtist"]) == 0 {
+			if len(Track["Artist"]) == 0 {
+				Artist = UnknownArtist
+				ArtistsFound = append(ArtistsFound, Artist)
+			} else {
+				Artist = Track["Artist"]
+			}
+		} else {
+			Artist = Track["AlbumArtist"]
 		}
 
-		if !contains(ArtistsFound, Track["AlbumArtist"]) {
-			lines = append(lines, fmt.Sprintf("%s", Track["AlbumArtist"]))
-			ArtistsFound = append(ArtistsFound, Track["AlbumArtist"])
+		if !contains(ArtistsFound, Artist) {
+			lines = append(lines, fmt.Sprintf("%s", Artist))
+			ArtistsFound = append(ArtistsFound, Artist)
 		}
 
-		if !contains(AlbumsFound, Track["Album"]) {
-			lines = append(lines, fmt.Sprintf("%s%s%s", Track["AlbumArtist"], Seperator, Track["Album"]))
-			AlbumsFound = append(AlbumsFound, Track["Album"])
+		var Album string
+		if len(Track["Album"]) != 0 {
+			Album = Track["Album"]
+		} else {
+			Album = UnknownAlbum
+			AlbumsFound = append(AlbumsFound, Album)
 		}
 
-		lines = append(lines, fmt.Sprintf("%s%s%s%s%s", Track["AlbumArtist"], Seperator, Track["Album"], Seperator, Track["Title"]))
+		if !contains(AlbumsFound, Album) {
+			lines = append(lines, fmt.Sprintf("%s%s%s", Artist, Seperator, Album))
+			AlbumsFound = append(AlbumsFound, Album)
+		}
+
+		if len(Track["Title"]) != 0 {
+			lines = append(lines, fmt.Sprintf("%s%s%s%s%s", Artist, Seperator, Album, Seperator, Track["Title"]))
+		}
 	}
 
 	subProcess := exec.Command("rofi", "-dmenu", "-p", "Name", "-i")
@@ -55,61 +78,58 @@ func main() {
 	stdin.Close()
 
 	var ID *mpd.PromisedId
-	var Added bool
+	Added := false
 
 	CommandList := client.BeginCommandList()
 	for scanner.Scan() {
 		Selection := strings.Split(scanner.Text(), Seperator)
-		if len(Selection) == 0 {
-			// No selection
-		} else if len(Selection) == 1 {
-			// Artist selection
-			for _, Track := range Tracks {
-				if Track["AlbumArtist"] == Selection[0] {
-					if Added {
-						CommandList.AddId(Track["file"], -1)
-					} else {
-						Added = true
-						ID = CommandList.AddId(Track["file"], -1)
-					}
-
-				}
-			}
-		} else if len(Selection) == 2 {
-			// Album selection
-			for _, Track := range Tracks {
-				if Track["AlbumArtist"] == Selection[0] &&
-					Track["Album"] == Selection[1] {
-					if Added {
-						CommandList.AddId(Track["file"], -1)
-					} else {
-						ID = CommandList.AddId(Track["file"], -1)
-						Added = true
-					}
-				}
-			}
-		} else if len(Selection) == 3 {
-			// Track selection
-			for _, Track := range Tracks {
-				if Track["AlbumArtist"] == Selection[0] &&
-					Track["Album"] == Selection[1] &&
-					Track["Title"] == Selection[2] {
-					ID = CommandList.AddId(Track["file"], -1)
-				}
-			}
-		} else {
-			// ???
+		CachedLength := len(Selection)
+		if CachedLength > 4 {
+			return
 		}
+
+		for _, Track := range Tracks {
+			_, HasAlbumArtist := Track["AlbumArtist"]
+			_, HasArtist := Track["Artist"]
+			ArtistUnknown := !HasAlbumArtist && !HasArtist
+
+			ArtistMatch :=
+				(Selection[0] == Track["AlbumArtist"] ||
+					Selection[0] == Track["Artist"]) ||
+					(Selection[0] == UnknownArtist &&
+						ArtistUnknown)
+
+			if !ArtistMatch {
+				continue
+			}
+
+			if CachedLength > 1 {
+				if Selection[1] != Track["Album"] &&
+					Selection[1] != UnknownAlbum {
+					continue
+				}
+				if CachedLength > 2 {
+					if Selection[2] != Track["Title"] {
+						continue
+					}
+				}
+			}
+
+			if Added {
+				CommandList.AddId(Track["file"], -1)
+			} else {
+				Added = true
+				ID = CommandList.AddId(Track["file"], -1)
+			}
+		}
+	}
+
+	if Added {
 		CommandList.End()
+		id, _ := ID.Value()
+		client.PlayId(id)
+		client.Pause(false)
 	}
-
-	if err == nil {
-		os.Exit(0)
-	}
-
-	id, _ := ID.Value()
-	client.PlayId(id)
-	client.Pause(false)
 }
 
 func contains(s []string, e string) bool {
@@ -121,8 +141,48 @@ func contains(s []string, e string) bool {
 	return false
 }
 
-func orPanic(err error) {
-	if err != nil {
-		panic(err)
-	}
+func GetBaseFile(in string) string {
+	paths := strings.Split(in, "/")
+	return paths[len(paths)-1]
 }
+
+/*
+
+	if len(Selection) == 1 {
+		// Artist selection
+		for _, Track := range Tracks {
+			if Track["AlbumArtist"] == Selection[0] {
+				if Added {
+					CommandList.AddId(Track["file"], -1)
+				} else {
+					Added = true
+					ID = CommandList.AddId(Track["file"], -1)
+				}
+
+			}
+		}
+	} else if len(Selection) == 2 {
+		// Album selection
+		for _, Track := range Tracks {
+			if Track["AlbumArtist"] == Selection[0] &&
+				Track["Album"] == Selection[1] {
+				if Added {
+					CommandList.AddId(Track["file"], -1)
+				} else {
+					ID = CommandList.AddId(Track["file"], -1)
+					Added = true
+				}
+			}
+		}
+	} else if len(Selection) == 3 {
+		// Track selection
+		for _, Track := range Tracks {
+			if (Track["AlbumArtist"] == Selection[0] ||
+				Track["Artist"] == Selection[0]) &&
+				Track["Album"] == Selection[1] &&
+				Track["Title"] == Selection[2] {
+				ID = CommandList.AddId(Track["file"], -1)
+			}
+		}
+	}
+*/
